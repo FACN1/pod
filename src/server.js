6df944c776d4b4362d33085e1026c27b271da845
philawsophizing <philawsophizing@gmail.com>
2017-04-30 17:54:16 +0300

    Create a hapi server in server.js and start server in start.js. Ref #19

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const hapi = require('hapi');
+const vision = require('vision');
+const routes = require('./routes/index.js');
+const inert = require('inert');
+const handlebars = require('handlebars');
+
+const server = new Hapi.Server();
+
+server.connection({
+  port: process.env.PORT || 3000,
+});
+
+server.register([ inert, vision ], (err) => {
+  if (err) throw err;
+
+  server.views({
+    engines: {
+      hbs: handlebars
+    },
+    relativeTo: __dirname,
+    helpersPath: './views/helpers',
+    path: './views',
+    layout: 'layout',
+    partialsPath: './views/partials',
+    layoutPath: './views/layout'
+  });
+
+  server.route(routes);
+})
+
+module.exports = server;
