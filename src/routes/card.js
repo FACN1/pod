fe7c41e9dfb6129a6b47b68fc0d39cb2dcddf958
Macintosh Helper <noreply@macintoshhelper.com>
2017-05-03 18:08:33 +0300

    [WIP] Implement detect OCR text with temp image for Cloud Vision - ref #60

--- src/routes/card.js
+++ src/routes/card.js
@@ -1,7 +1,34 @@
 const url = require('url');
+const path = require('path');
+const vision = require('@google-cloud/vision')({
+
+  // The path to key file:
+  keyFilename: './googleapikey.json',
+});
 
 const handler = (request, reply) => {
   const parsedUrl = url.parse(request.url);
+
+  // const vision = Vision();
+
+  const fileName = path.join(__dirname, '../../public/assets/images/visa.png');
+
+  vision.detectText(fileName)
+    .then((results) => {
+      const detections = results[0];
+
+      console.log('Image text: ');
+      detections.forEach(text => console.log(text));
+    })
+    .catch((err) => {
+      console.error('ERROR: ', err);
+    });
+/*
+  request.payload
+
+
+  parsedUrl.query.*/
+
   if (parsedUrl.query.ajax) reply.view('card', {}, { layout: 'spa' });
   else reply.view('card');
 };
