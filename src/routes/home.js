739d1deef6c8692cb0fd3ba9371246405d552f21
Macintosh Helper <noreply@macintoshhelper.com>
2017-05-03 11:59:09 +0300

    Call dbQueries getShopCoord function in home.js and give hbs the coordinate object - ref #51

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,21 @@
 const url = require('url');
+const dbQueries = require('../dbQueries.js');
 
 const handler = (request, reply) => {
-  const parsedUrl = url.parse(request.url);
-  if (parsedUrl.query.ajax) reply.view('index', {}, { layout: 'spa' });
-  else reply.view('index');
+  dbQueries.getShopCoordinates(1, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const data = {
+      shopCoordinates: JSON.stringify(res.rows[0]),
+    };
+
+    const parsedUrl = url.parse(request.url);
+    if (parsedUrl.query.ajax) reply.view('index', data, { layout: 'spa' });
+    else reply.view('index', data);
+  });
 };
 
 module.exports = {
