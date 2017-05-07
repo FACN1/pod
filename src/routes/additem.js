5c64fa2565740462dab2bda505dbea6c52b5aadd
Macintosh Helper <noreply@macintoshhelper.com>
2017-05-07 16:20:31 +0300

    [WIP] Submit uploaded barcode to Cloud Vision and log the response - ref #66

---
+++ src/routes/additem.js
@@ -0,0 +1,59 @@
+// const url = require('url');
+// const path = require('path');
+const vision = require('@google-cloud/vision')({
+
+  // The path to key file:
+  keyFilename: './googleapikey.json',
+});
+
+const handler = (request, reply) => {
+  // const parsedUrl = url.parse(request.url);
+
+  // const vision = Vision();
+
+  // // const fileName = path.join(__dirname, '../../public/assets/images/visa.png');
+  // console.log('Request payload: ', request.payload);
+  // // var encoded = new Buffer(imageFile).toString('base64');
+  if (request.auth.isAuthenticated) {
+    vision.detectText(request.payload.path)
+      .then((results) => {
+        const detections = results[0];
+
+        const detectionsText = detections.join('');
+
+        const barcodeNumber = detectionsText.match('[0-9]{11}([0-9]{2})?')[0];
+        console.log('Image text: ', barcodeNumber);
+
+        return reply(JSON.stringify({
+          token: request.headers.authorization,
+        }));
+      })
+      .catch((err) => {
+        console.error('ERROR: ', err);
+      });
+  } else {
+    reply('Not authorized');
+  }
+/*
+  request.payload
+  parsedUrl.query.*/
+
+  // if (parsedUrl.query.ajax) reply.view('card', {}, { layout: 'spa' });
+  // else reply.view('card');
+};
+
+module.exports = {
+  method: 'POST',
+  path: '/add-item',
+  config: {
+    handler,
+    payload: {
+      maxBytes: 209715200,
+      output: 'file',
+      parse: true,
+    },
+    auth: {
+      mode: 'optional',
+    },
+  },
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const staticHandler = require('./static');
 const homeHandler = require('./home.js');
 const cardHandler = require('./card.js');
 const cartHandler = require('./cart.js');
+const addItemHandler = require('./additem.js');
 
 module.exports = [
   staticHandler,
   homeHandler,
   cardHandler,
   cartHandler,
+  addItemHandler,
 ];
