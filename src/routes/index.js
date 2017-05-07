df6ba609132bb55fa35c68b2d2508caea3e1649e
Macintosh Helper <noreply@macintoshhelper.com>
2017-05-07 11:23:46 +0300

    Export cartHandler from router (index.js) - ref #54

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const staticHandler = require('./static');
 const homeHandler = require('./home.js');
 const cardHandler = require('./card.js');
+const cartHandler = require('./cart.js');
 
 module.exports = [
   staticHandler,
   homeHandler,
   cardHandler,
+  cartHandler,
 ];
